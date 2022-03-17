2855131834e86dd44079b75735cc00185b347ccd
Vladislavs Perkanuks <v.perkanuks@gmail.com>
2022-03-17 12:01:43 +0200

    Final changes

--- src/stepper.cpp
+++ src/stepper.cpp
@@ -1,7 +1,7 @@
 #include "stepper.h"
 #include "web_server.h"
 
-const uint32_t DISTANCE_NM_PER_ROTATION = 326000;
+const uint32_t DISTANCE_NM_PER_ROTATION = 320800;
 static const uint8_t Q1[] = {LOW, HIGH, HIGH, LOW};
 static const uint8_t Q2[] = {HIGH, LOW, LOW, HIGH};
 static const uint8_t Q3[] = {LOW, LOW, HIGH, HIGH};
@@ -9,7 +9,7 @@ static const uint8_t Q4[] = {HIGH, HIGH, LOW, LOW};
 
 uint16_t calculate_pause_interval(uint32_t speed)
 {
-  return round((double)DISTANCE_NM_PER_ROTATION / double(speed));
+  return round(DISTANCE_NM_PER_ROTATION / double(speed));
 }
 
 uint32_t calculate_time_needed(uint32_t distance, uint32_t speed)
@@ -48,12 +48,12 @@ void make_step(int phase)
 
 bool go_up_(unsigned long currentMillis, unsigned long *previousMillis, uint16_t *position)
 {
-  if (currentMillis - *previousMillis >= 25)
+  if (currentMillis - *previousMillis >= 65)
   {
     int ledstate = (*position) % 2 == 0 ? HIGH : LOW;
     digitalWrite(LEDPIN, ledstate);
     make_step((*position)++ & 0x3);
     *previousMillis = currentMillis;
   }
-  return digitalRead(REACHED_TOP_LINE);
+  return !digitalRead(REACHED_TOP_LINE);
 }
