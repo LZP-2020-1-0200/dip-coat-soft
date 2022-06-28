bb72ef3f9be54ffc05cd5ede865f687962486267
Vladislavs Perkanuks <v.perkanuks@gmail.com>
2022-06-28 13:02:16 +0300

    Cleanup + go down functionality

--- src/stepper.cpp
+++ src/stepper.cpp
@@ -1,26 +1,15 @@
 #include "stepper.h"
 #include "web_server.h"
 
-// const uint32_t DISTANCE_NM_PER_ROTATION = 320800;
 const double DISTANCE_NM_PER_ROTATION = 83333.33;
 static const uint8_t Q1[] = {LOW, HIGH, HIGH, LOW};
 static const uint8_t Q2[] = {HIGH, LOW, LOW, HIGH};
 static const uint8_t Q3[] = {LOW, LOW, HIGH, HIGH};
 static const uint8_t Q4[] = {HIGH, HIGH, LOW, LOW};
 
-// unsigned long calculate_pause_interval(uint32_t speed)
-// {
-//   return round(DISTANCE_NM_PER_ROTATION / double(speed)*1000);
-// }
-
-// uint64_t calculate_time_needed(uint32_t distance, uint32_t speed)
-// {
-//   return round(((double)distance / (double)speed) * 1000);
-// }
-
 uint32_t calculate_pause_interval(uint32_t speed)
 {
-  return round(DISTANCE_NM_PER_ROTATION / double(speed)*1000);
+  return round(DISTANCE_NM_PER_ROTATION / double(speed) * 1000);
 }
 
 uint64_t calculate_time_needed(uint32_t distance, uint32_t speed)
@@ -38,13 +27,30 @@ void make_step(uint16_t phase)
 
 bool go_up_(unsigned long currentMillis, unsigned long *previousMillis, uint16_t *position)
 {
+  digitalWrite(UP_LED, HIGH);
   if (currentMillis - *previousMillis >= 4800)
   {
-    // int ledstate = (*position) % 2 == 0 ? HIGH : LOW;
-    // digitalWrite(LEDPIN, ledstate);
-    // Inverted
     make_step((*position)-- & 0x3);
     *previousMillis = currentMillis;
   }
+
+  if (digitalRead(REACHED_TOP_LINE) == false)
+    digitalWrite(UP_LED, LOW);
+
   return !digitalRead(REACHED_TOP_LINE);
+}
+
+bool go_down_(unsigned long currentMillis, unsigned long *previousMillis, uint16_t *position)
+{
+  digitalWrite(DOWN_LED, HIGH);
+  if (currentMillis - *previousMillis >= 4800)
+  {
+    make_step((*position)++ & 0x3);
+    *previousMillis = currentMillis;
+  }
+
+  if (digitalRead(REACHED_BOTTOM_LINE) == false)
+    digitalWrite(DOWN_LED, LOW);
+
+  return !digitalRead(REACHED_BOTTOM_LINE);
 }
